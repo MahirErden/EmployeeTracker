5ee84fad36c5222b99b949eb105133a44afd146d
Mahir Erden <mahirerden@gmail.com>
2020-01-28 15:09:26 -0500

    homework completed

--- app.js
+++ app.js
@@ -172,13 +172,12 @@ function viewEmployeesByManager() {
   connection.query(query, function (err, res) {
     if (err) throw err;
     array = JSON.parse(JSON.stringify(res));
-    console.log(array);
 
     inquirer
       .prompt({
         name: 'manager',
         type: 'list',
-        message: 'Which department\'s employees do you want to see?',
+        message: 'Which Manager\'s employees do you want to see?',
         choices: array
       })
       .then(function (answer) {
@@ -255,7 +254,8 @@ function addRole() {
       {
         type: "input",
         name: "salary",
-        message: "What is the salary of this new role?"
+        message: "What is the salary of this new role?",
+        validate: validateSalary
       },
       {
         type: 'list',
@@ -284,218 +284,258 @@ function addRole() {
   });
 }
 
+function validateSalary(salary) {
+  var reg = /^\d+$/;
+  return reg.test(salary) || "Salary should be a number!";
+}
+
 function addEmployee() {
   //let array = [];
   inquirer.prompt([
-      {
-        type: "input",
-        name: "first_name",
-        message: "What is your first name?"
-      },
-      {
-        type: "input",
-        name: "last_name",
-        message: "What is your last name?"
-      }
-    ]).then(function (answer) {
-        var query = "SELECT role_id as value, role_title as name FROM role WHERE manager = 0";
-        connection.query(query, function (err, res) {
-          if (err) throw err;
-          let array = JSON.parse(JSON.stringify(res));
-          inquirer
-            .prompt(
-              {
-                name: 'role',
-                type: 'list',
-                message: 'Choose a role for the new employee',
-                choices: array
-              }).then(function (answer1) {
-                var query = "SELECT employee.employee_id as value, CONCAT(employee.first_name, ' ', employee.last_name) as name " +
-                  "FROM employee INNER JOIN role ON employee.role_id = role.role_id WHERE role.manager = 1";
-                connection.query(query, function (err, res) {
-                  if (err) throw err;
-                  let array2 = JSON.parse(JSON.stringify(res));
-                  inquirer
-                    .prompt({
-                      name: 'manager',
-                      type: 'list',
-                      message: 'Choose a manager for the new employee',
-                      choices: array2
-                    }).then(function (answer2) {
-                      connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ( ?, ?, ?, ?)",
-                        [answer.first_name, answer.last_name, answer1.role, answer2.manager], function (err, res) {
-                          if (err) throw err;
-                          if (res.affectedRows > 0) {
-                            console.log(res.affectedRows + " record added successfully!");
-                          }
-                          console.log("");
-                          init();
-                        });
+    {
+      type: "input",
+      name: "first_name",
+      message: "What is your first name?"
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "What is your last name?"
+    }
+  ]).then(function (answer) {
+    var query = "SELECT role_id as value, role_title as name FROM role WHERE manager = 0";
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+      let array = JSON.parse(JSON.stringify(res));
+      inquirer
+        .prompt(
+          {
+            name: 'role',
+            type: 'list',
+            message: 'Choose a role for the new employee',
+            choices: array
+          }).then(function (answer1) {
+            var query = "SELECT employee.employee_id as value, CONCAT(employee.first_name, ' ', employee.last_name) as name " +
+              "FROM employee INNER JOIN role ON employee.role_id = role.role_id WHERE role.manager = 1";
+            connection.query(query, function (err, res) {
+              if (err) throw err;
+              let array2 = JSON.parse(JSON.stringify(res));
+              inquirer
+                .prompt({
+                  name: 'manager',
+                  type: 'list',
+                  message: 'Choose a manager for the new employee',
+                  choices: array2
+                }).then(function (answer2) {
+                  connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ( ?, ?, ?, ?)",
+                    [answer.first_name, answer.last_name, answer1.role, answer2.manager], function (err, res) {
+                      if (err) throw err;
+                      if (res.affectedRows > 0) {
+                        console.log(res.affectedRows + " record added successfully!");
+                      }
+                      console.log("");
+                      init();
                     });
                 });
-              });
-        });
-     });
+            });
+          });
+    });
+  });
 }
 
 function updateEmployeeRole() {
-          //let array = [];
-          var query = "SELECT employee.employee_id as value, " +
-            "CONCAT(employee.first_name, ' ', employee.last_name) as name FROM employee WHERE manager_id IS NOT NULL";
-          connection.query(query, function (err, res) {
-            if (err) throw err;
-            let array = JSON.parse(JSON.stringify(res));
-            inquirer
-              .prompt({
-                name: 'employee',
-                type: 'list',
-                message: 'Which employee\'s role do you want to change?',
-                choices: array
-              }).then(function (answer1) {
-                var query = "SELECT role_id as value, role_title as name FROM role WHERE manager = 0";
-                connection.query(query, function (err, res) {
-                  if (err) throw err;
-                  let array2 = JSON.parse(JSON.stringify(res));
-                  inquirer
-                    .prompt({
-                      name: 'role',
-                      type: 'list',
-                      message: 'Which is the new role?',
-                      choices: array2
-                    }).then(function (answer2) {
-                      connection.query("UPDATE employee SET role_id = ? WHERE employee_id = ?",
-                        [answer2.role, answer1.employee], function (err, res) {
-                          if (err) throw err;
-                          if (res.affectedRows > 0) {
-                            console.log(res.affectedRows + " record updated successfully!");
-                          }
-                          console.log("");
-                          init();
-                        });
-                    });
+  //let array = [];
+  var query = "SELECT employee.employee_id as value, " +
+    "CONCAT(employee.first_name, ' ', employee.last_name) as name FROM employee WHERE manager_id IS NOT NULL";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    let array = JSON.parse(JSON.stringify(res));
+    inquirer
+      .prompt({
+        name: 'employee',
+        type: 'list',
+        message: 'Which employee\'s role do you want to change?',
+        choices: array
+      }).then(function (answer1) {
+        var query = "SELECT role_id as value, role_title as name FROM role WHERE manager = 0";
+        connection.query(query, function (err, res) {
+          if (err) throw err;
+          let array2 = JSON.parse(JSON.stringify(res));
+          inquirer
+            .prompt({
+              name: 'role',
+              type: 'list',
+              message: 'Which is the new role?',
+              choices: array2
+            }).then(function (answer2) {
+              connection.query("UPDATE employee SET role_id = ? WHERE employee_id = ?",
+                [answer2.role, answer1.employee], function (err, res) {
+                  if (err) {
+                    if (err.errno === 1451){
+                      console.log("You cannot delete this record because of foreign key constrait!");
+                    }else{
+                      console.log("An error occured!");
+                    }
+                    return init();
+                  }
+                  if (res.affectedRows > 0) {
+                    console.log(res.affectedRows + " record updated successfully!");
+                  }
+                  console.log("");
+                  init();
                 });
-              });
-          });
-        }
+            });
+        });
+      });
+  });
+}
 
 function updateEmployeeManager() {
-          //let array = [];
-          var query = "SELECT employee.employee_id as value, " +
-            "CONCAT(employee.first_name, ' ', employee.last_name) as name FROM employee WHERE manager_id IS NOT NULL";
-          connection.query(query, function (err, res) {
-            if (err) throw err;
-            let array = JSON.parse(JSON.stringify(res));
-            inquirer
-              .prompt({
-                name: 'employee',
-                type: 'list',
-                message: 'Which employee\'s manager do you want to change?',
-                choices: array
-              }).then(function (answer1) {
-                var query = "SELECT employee.employee_id as value, CONCAT(employee.first_name, ' ', employee.last_name) as name " +
-                  "FROM employee INNER JOIN role ON employee.role_id = role.role_id WHERE role.manager = 1";
-                connection.query(query, function (err, res) {
-                  if (err) throw err;
-                  let array2 = JSON.parse(JSON.stringify(res));
-                  inquirer
-                    .prompt({
-                      name: 'role',
-                      type: 'list',
-                      message: 'Who is the new manager?',
-                      choices: array2
-                    }).then(function (answer2) {
-                      connection.query("UPDATE employee SET role_id = ? WHERE employee_id = ?",
-                        [answer2.role, answer1.employee], function (err, res) {
-                          if (err) throw err;
-                          if (res.affectedRows > 0) {
-                            console.log(res.affectedRows + " record updated successfully!");
-                          }
-                          console.log("");
-                          init();
-                        });
-                    });
+  //let array = [];
+  var query = "SELECT employee.employee_id as value, " +
+    "CONCAT(employee.first_name, ' ', employee.last_name) as name FROM employee WHERE manager_id IS NOT NULL";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    let array = JSON.parse(JSON.stringify(res));
+    inquirer
+      .prompt({
+        name: 'employee',
+        type: 'list',
+        message: 'Which employee\'s manager do you want to change?',
+        choices: array
+      }).then(function (answer1) {
+        var query = "SELECT employee.employee_id as value, CONCAT(employee.first_name, ' ', employee.last_name) as name " +
+          "FROM employee INNER JOIN role ON employee.role_id = role.role_id WHERE role.manager = 1";
+        connection.query(query, function (err, res) {
+          if (err) throw err;
+          let array2 = JSON.parse(JSON.stringify(res));
+          inquirer
+            .prompt({
+              name: 'role',
+              type: 'list',
+              message: 'Who is the new manager?',
+              choices: array2
+            }).then(function (answer2) {
+              connection.query("UPDATE employee SET manager_id = ? WHERE employee_id = ?",
+                [answer2.role, answer1.employee], function (err, res) {
+                  if (err) {
+                    if (err.errno === 1451){
+                      console.log("You cannot delete this record because of foreign key constrait!");
+                    }else{
+                      console.log("An error occured!");
+                    }
+                    return init();
+                  }
+                  if (res.affectedRows > 0) {
+                    console.log(res.affectedRows + " record updated successfully!");
+                  }
+                  console.log("");
+                  init();
                 });
-              });
-          });
-        }
+            });
+        });
+      });
+  });
+}
 
 function removeDepartment() {
-          let array = [];
-          var query = "SELECT department_id as value, department_name as name FROM department";
-          connection.query(query, function (err, res) {
-            if (err) throw err;
-            array = JSON.parse(JSON.stringify(res));
+  let array = [];
+  var query = "SELECT department_id as value, department_name as name FROM department";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    array = JSON.parse(JSON.stringify(res));
 
-            inquirer
-              .prompt({
-                name: 'department',
-                type: 'list',
-                message: 'Which department do want to remove?',
-                choices: array
-              })
-              .then(function (answer) {
-                connection.query("DELETE from department WHERE department_id = ?", [answer.department], function (err, res) {
-                  if (err) throw err;
-                  if (res.affectedRows > 0) {
-                    console.log(res.affectedRows + " record deleted successfully!");
-                  }
-                  console.log("");
-                  init();
-                });
-              });
+    inquirer
+      .prompt({
+        name: 'department',
+        type: 'list',
+        message: 'Which department do you want to remove?',
+        choices: array
+      })
+      .then(function (answer) {
+          connection.query("DELETE from department WHERE department_id = ?", [answer.department], function (err, res) {
+            if (err) {
+              if (err.errno === 1451){
+                console.log("You cannot delete this record because of foreign key constrait!");
+              }else{
+                console.log("An error occured!");
+              }
+              return init();
+            }
+            if (res.affectedRows > 0) {
+              console.log(res.affectedRows + " record deleted successfully!");
+            }
+            console.log("");
+            init();
           });
-        }
+      });
+  });
+}
 
 function removeRole() {
-          let array = [];
-          var query = "SELECT role_id as value, role_title as name FROM role";
-          connection.query(query, function (err, res) {
-            if (err) throw err;
-            array = JSON.parse(JSON.stringify(res));
+  let array = [];
+  var query = "SELECT role_id as value, role_title as name FROM role";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    array = JSON.parse(JSON.stringify(res));
 
-            inquirer
-              .prompt({
-                name: 'role',
-                type: 'list',
-                message: 'Which role do want to remove?',
-                choices: array
-              })
-              .then(function (answer) {
-                connection.query("DELETE from role WHERE role_id = ?", [answer.role], function (err, res) {
-                  if (err) throw err;
-                  if (res.affectedRows > 0) {
-                    console.log(res.affectedRows + " record deleted successfully!");
-                  }
-                  console.log("");
-                  init();
-                });
-              });
-          });
-        }
+    inquirer
+      .prompt({
+        name: 'role',
+        type: 'list',
+        message: 'Which role do want to remove?',
+        choices: array
+      })
+      .then(function (answer) {
+        connection.query("DELETE from role WHERE role_id = ?", [answer.role], function (err, res) {
+          if (err) {
+            if (err.errno === 1451){
+              console.log("You cannot delete this record because of foreign key constrait!");
+            }else{
+              console.log("An error occured!");
+            }
+            return init();
+          }
+          if (res.affectedRows > 0) {
+            console.log(res.affectedRows + " record deleted successfully!");
+          }
+          console.log("");
+          init();
+        });
+      });
+  });
+}
 
 function removeEmployee() {
   var query = "SELECT employee.employee_id as value, CONCAT(employee.first_name, ' ', employee.last_name) as name " +
-  "FROM employee INNER JOIN role ON employee.role_id = role.role_id WHERE role.manager = 0";
+    "FROM employee INNER JOIN role ON employee.role_id = role.role_id WHERE role.manager = 0";
   connection.query(query, function (err, res) {
-  if (err) throw err;
-  let array = JSON.parse(JSON.stringify(res));
-  inquirer
-    .prompt({
-      name: 'employee',
-      type: 'list',
-      message: 'Which employee do you want to remove?',
-      choices : array
-    }).then(function (answer) {
-      connection.query("DELETE from employee WHERE employee_id = ?", [answer.employee], function (err, res) {
-        if (err) throw err;
-        if (res.affectedRows > 0) {
-          console.log(res.affectedRows + " record deleted successfully!");
-        }
-        console.log("");
-        init();
+    if (err) throw err;
+    let array = JSON.parse(JSON.stringify(res));
+    inquirer
+      .prompt({
+        name: 'employee',
+        type: 'list',
+        message: 'Which employee do you want to remove?',
+        choices: array
+      }).then(function (answer) {
+        connection.query("DELETE from employee WHERE employee_id = ?", [answer.employee], function (err, res) {
+          if (err) {
+            if (err.errno === 1451){
+              console.log("You cannot delete this record because of foreign key constrait!");
+            }else{
+              console.log("An error occured!");
+            }
+            return init();
+          }
+          if (res.affectedRows > 0) {
+            console.log(res.affectedRows + " record deleted successfully!");
+          }
+          console.log("");
+          init();
+        });
       });
-    }); 
-  });     
+  });
 }
 
 
